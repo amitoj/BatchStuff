ca1c4f79aa10f5518390134b9ca7af665e414af1
Mauro Scomparin <scompo@gmail.com>
2015-01-09 19:25:00 +0100

    added failing test

---
+++ src/test/java/it/scompo/batchstuff/test/application/ApplicationTest.java
@@ -0,0 +1,14 @@
+package it.scompo.batchstuff.test.application;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
