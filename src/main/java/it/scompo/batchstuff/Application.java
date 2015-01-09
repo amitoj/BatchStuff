e422d9d79ed78152212e027170f0fd64d7a2d323
Mauro Scomparin <scompo@gmail.com>
2015-01-09 19:42:10 +0100

    added annotations

--- src/main/java/it/scompo/batchstuff/Application.java
+++ src/main/java/it/scompo/batchstuff/Application.java
@@ -1,9 +1,16 @@
 package it.scompo.batchstuff;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
 public class Application {
 
 	public static void main(String[] args) {
 		
+		SpringApplication.run(Application.class, args);
 		
 	}
 
