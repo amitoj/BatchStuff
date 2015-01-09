3e64d8c7e11aba7436a3a709a94eae2d71c22986
Mauro Scomparin <scompo@gmail.com>
2015-01-09 19:45:30 +0100

    added batch configuration

---
+++ src/main/java/it/scompo/batchstuff/config/BatchConfiguration.java
@@ -0,0 +1,10 @@
+package it.scompo.batchstuff.config;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfiguration {
+
+}
