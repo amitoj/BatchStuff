c48218e548c251fc87bf49a071d59f916b646826
Mauro Scomparin <scompo@gmail.com>
2015-01-09 21:32:16 +0100

    added logger configuration for application

--- src/main/java/it/scompo/batchstuff/batch/nothing/steps/NothingOnlyStepTasklet.java
+++ src/main/java/it/scompo/batchstuff/batch/nothing/steps/NothingOnlyStepTasklet.java
@@ -1,5 +1,7 @@
 package it.scompo.batchstuff.batch.nothing.steps;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -8,13 +10,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class NothingOnlyStepTasklet  implements Tasklet{
+	
+	private static final Logger logger = LoggerFactory.getLogger(NothingOnlyStepTasklet.class);
 
 	@Override
 	public RepeatStatus execute(StepContribution contribution,
 			ChunkContext chunkContext) throws Exception {
 		
-		System.err.println("doing nothing");
+		doNothing();
+		
 		return RepeatStatus.FINISHED;
+	}
+	
+	public static void doNothing(){
+		
+		logger.info("doing nothing..");
 	}
 
 }
