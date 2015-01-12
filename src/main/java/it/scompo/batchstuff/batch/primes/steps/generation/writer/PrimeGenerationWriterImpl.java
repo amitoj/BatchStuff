bbd7f812361065da83a5996f5537880c93aaac96
Mauro Scomparin <scompo@gmail.com>
2015-01-12 23:08:19 +0100

    changed logging levels

--- src/main/java/it/scompo/batchstuff/batch/primes/steps/generation/writer/PrimeGenerationWriterImpl.java
+++ src/main/java/it/scompo/batchstuff/batch/primes/steps/generation/writer/PrimeGenerationWriterImpl.java
@@ -3,18 +3,23 @@ package it.scompo.batchstuff.batch.primes.steps.generation.writer;
 import java.math.BigInteger;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PrimeGenerationWriterImpl implements PrimeGenerationWriter {
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(PrimeGenerationWriterImpl.class);
+
 	@Override
 	public void write(List<? extends BigInteger> items) throws Exception {
 
-		System.err.println("Size=" + items.size());
+		logger.info("Writing " + items.size() + " primes");
 
 		for (BigInteger bigInteger : items) {
-			System.err.println("NUM= " + bigInteger);
+			logger.debug("NUM= " + bigInteger);
 		}
 	}
 
