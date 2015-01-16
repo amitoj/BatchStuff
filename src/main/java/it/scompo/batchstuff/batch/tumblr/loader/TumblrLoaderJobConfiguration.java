bac750977df8554926a9d46160f4cda3b9a22ce8
Mauro Scomparin <scompo@gmail.com>
2015-01-16 21:02:59 +0100

    Added configuration for first step

--- src/main/java/it/scompo/batchstuff/batch/tumblr/loader/TumblrLoaderJobConfiguration.java
+++ src/main/java/it/scompo/batchstuff/batch/tumblr/loader/TumblrLoaderJobConfiguration.java
@@ -1,5 +1,31 @@
 package it.scompo.batchstuff.batch.tumblr.loader;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class TumblrLoaderJobConfiguration {
 
+	public static final String JOB_NAME = "tumblrJobName";
+
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	private Step tumblrLoadUserStep;
+
+	@Autowired
+	private Step tumblrLoadBlogsStep;
+
+	@Autowired
+	private Step tumblrLoadPostsStep;
+
+	public Job tumblrJob() {
+
+		return jobBuilderFactory.get(JOB_NAME).start(tumblrLoadUserStep)
+				.next(tumblrLoadBlogsStep).next(tumblrLoadPostsStep).build();
+	}
 }
